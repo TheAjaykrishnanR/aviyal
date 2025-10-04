c843ebca947fcd71e760d79411835aa30908c453
AjaykrishnanR <theconsciousstoictaoistcat@gmail.com>
2025-10-05 00:12:15 +0530

    margins

--- Main.cs
+++ Main.cs
@@ -39,7 +39,6 @@ public class Window : IWindow
 		{
 			User32.GetWindowRect(this.hWnd, out RECT _rect);
 			return _rect;
-
 		}
 
 	}
@@ -80,13 +79,13 @@ public class Window : IWindow
 	public RECT GetFrameMargin()
 	{
 		User32.GetWindowRect(this.hWnd, out RECT rect);
-		Console.WriteLine($"GWR [L: {rect.Left} R: {rect.Right} T: {rect.Top} B:{rect.Bottom}]");
+		//Console.WriteLine($"GWR [L: {rect.Left} R: {rect.Right} T: {rect.Top} B:{rect.Bottom}]");
 		int size = Marshal.SizeOf<RECT>();
 		nint rectPtr = Marshal.AllocHGlobal(size);
 		Dwmapi.DwmGetWindowAttribute(this.hWnd, (uint)DWMWINDOWATTRIBUTE.DWMWA_EXTENDED_FRAME_BOUNDS, rectPtr, (uint)size);
 		RECT rect2 = Marshal.PtrToStructure<RECT>(rectPtr);
 		Marshal.FreeHGlobal(rectPtr);
-		Console.WriteLine($"DWM [L: {rect2.Left} R: {rect2.Right} T: {rect2.Top} B:{rect2.Bottom}]");
+		//Console.WriteLine($"DWM [L: {rect2.Left} R: {rect2.Right} T: {rect2.Top} B:{rect2.Bottom}]");
 
 		return new RECT()
 		{
@@ -138,31 +137,41 @@ public class Dwindle : ILayout
 {
 	public RECT[] GetRect(int count)
 	{
+		// rects with margin
 		RECT[] rects = new RECT[count];
+		// rects with no margins
+		RECT[] fillRects = new RECT[count];
 		(int width, int height) = Utils.GetScreenSize();
 		FillDirection fillDirection = FillDirection.HORIZONTAL;
 		// where the nth window will go
 		RECT fillRect = new() { Left = 0, Top = 0, Right = width, Bottom = height };
 		for (int i = 0; i < count; i++)
 		{
-			rects[i] = fillRect;
+			fillRects[i] = fillRect;
 
 			// modify the fillRect
 			switch (fillDirection)
 			{
 				case FillDirection.HORIZONTAL:
-					if (i - 1 >= 0) rects[i - 1] = TopHalf(rects[i - 1]);
+					if (i - 1 >= 0)
+					{
+						fillRects[i - 1] = TopHalf(fillRects[i - 1]);
+					}
 					fillRect.Left += (int)((fillRect.Right - fillRect.Left) / 2);
 					break;
 				case FillDirection.VERTICAL:
-					if (i - 1 >= 0) rects[i - 1] = LeftHalf(rects[i - 1]);
+					if (i - 1 >= 0)
+					{
+						fillRects[i - 1] = LeftHalf(fillRects[i - 1]);
+					}
 					fillRect.Top += (int)((fillRect.Bottom - fillRect.Top) / 2);
 					break;
 			}
 			fillDirection = fillDirection == FillDirection.HORIZONTAL ? FillDirection.VERTICAL : FillDirection.HORIZONTAL;
+
 		}
-		rects.Index().ToList().ForEach(irect => Console.WriteLine($"{irect.Item1}. L:{irect.Item2.Left} R:{irect.Item2.Right} T:{irect.Item2.Top} B:{irect.Item2.Bottom}"));
-		return rects;
+		fillRects.Index().ToList().ForEach(irect => Console.WriteLine($"{irect.Item1}. L:{irect.Item2.Left} R:{irect.Item2.Right} T:{irect.Item2.Top} B:{irect.Item2.Bottom}"));
+		return ApplyInner(ApplyOuter(fillRects));
 	}
 	public int outer { get; set; } = 5;
 	public int inner { get; set; } = 5;
@@ -178,8 +187,33 @@ public class Dwindle : ILayout
 		return rect;
 	}
 
-	void ApplyOuter(RECT[] rects) { }
-	void ApplyInner(RECT[] rects) { }
+	// applies outer margins
+	RECT[] ApplyOuter(RECT[] fillRects)
+	{
+		(int width, int height) = Utils.GetScreenSize();
+		for (int i = 0; i < fillRects.Length; i++)
+		{
+			if (fillRects[i].Left == 0) fillRects[i].Left += outer;
+			if (fillRects[i].Top == 0) fillRects[i].Top += outer;
+			if (fillRects[i].Right == width) fillRects[i].Right -= outer;
+			if (fillRects[i].Bottom == height) fillRects[i].Bottom -= outer;
+		}
+		return fillRects;
+	}
+
+	// applies inner margins (apply only after outer)
+	RECT[] ApplyInner(RECT[] fillRects)
+	{
+		(int width, int height) = Utils.GetScreenSize();
+		for (int i = 0; i < fillRects.Length; i++)
+		{
+			if (fillRects[i].Left != outer) fillRects[i].Left += (int)(inner / 2);
+			if (fillRects[i].Top != outer) fillRects[i].Top += (int)(inner / 2);
+			if (fillRects[i].Right != width - outer) fillRects[i].Right -= (int)(inner / 2);
+			if (fillRects[i].Bottom != height - outer) fillRects[i].Bottom -= (int)(inner / 2);
+		}
+		return fillRects;
+	}
 }
 
 public class WindowManager : IWindowManager
